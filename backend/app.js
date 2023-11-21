63d8cfb2450a18ae7b8c77ad8308a856021e4581
Prabhu Satyam <61291869+prabhu4444@users.noreply.github.com>
2023-11-21 14:33:11 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -46,9 +46,12 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, OPTIONS, DELETE');
 
   // Preflight CORS handler
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end(); // Send an empty response
-  }
+  if(req.method === 'OPTIONS') {
+        return res.status(200).json(({
+            body: "OK"
+        }))
+    }
+
 
   // Call next() to move to the next middleware or route handler
   next();
